6df4875a650b1cf7b9a55ef5b495e9fedc030538
root <root@localhost.localdomain>
2025-08-14 17:13:01 -0400

    init

---
+++ api_handler.py
@@ -0,0 +1,13 @@
+from classes import ApiHandler
+
+api_handler = ApiHandler()
+
+api_handler.get_load_stats()
+api_handler.submit_load_stats()
+
+if api_handler.response_data.get('scan_requested', False):
+    api_handler.submit_log_data()
+
+if api_handler.response_data.get('blocked_ips', False) and api_handler.response_data.get('whitelisted_ips', False):  
+    api_handler.process_blocks()
+    
---
+++ classes.py
@@ -0,0 +1,174 @@
+from datetime import timedelta, datetime
+from subprocess import CalledProcessError, check_output, run
+from dotenv import load_dotenv
+import os
+import requests
+
+class LogParser:
+    def __init__(self, 
+                 minutes = "30", 
+                 path = "", 
+                 domain = "*", 
+                 log_type = "*access_ssl_log", 
+                 code = ""):
+        self.path = path
+        self.domain = domain
+        self.minutes = int(minutes)
+        self.log_type = log_type
+        self.code = code
+        # set time range
+        self.end_time = datetime.now()
+        self.start_time = self.end_time - timedelta(minutes=self.minutes)
+
+        # set log string to check, all logs by default - nginx and apache
+        self.log_string = {
+            'main': 'access_ssl_log',
+            'proxy': 'proxy_access_ssl_log',
+            'all': '*access_ssl_log'
+        }.get(self.log_type, '*access_ssl_log')
+
+        # check if custom log path is provided
+        if self.path != "":
+            self.output = check_output(f"ls -1 {self.path}",
+                                  shell=True, universal_newlines=True)
+        else:
+            self.output = check_output(f"ls -1 /var/www/vhosts/system/{self.domain}/logs/{self.log_string}",
+                                  shell=True, universal_newlines=True)
+
+        self.log_files = self.output.strip().split('\n')
+
+    def get_log_time(self,line):
+        """ Extract timestamp from the log line in the format [dd/Mon/yyyy:HH:MM:SS +0000] """
+        try:
+            timestamp_str = line.split("[")[1].split("]")[0].split(" ")[0]
+            return datetime.strptime(timestamp_str, "%d/%b/%Y:%H:%M:%S")
+        except Exception:
+            return None
+
+    def parse_logs(self):
+        self.filtered_logs = []
+        self.requests_by_domain = {}
+
+        for i, log_file in enumerate(self.log_files, 1):
+            # Get domain name from log path
+            try:
+                domain_name = log_file.split('/system/')[1].split('/')[0]
+            except:
+                domain_name = "not found"
+
+            self.requests_by_domain.setdefault(domain_name, {'count': 0, 'uniq_ips': set()})
+
+            # Load log file
+            with open(log_file, 'r') as f:
+                log_lines = f.readlines()
+
+            # Filter log lines in time range
+            for line in log_lines:
+                log_time = self.get_log_time(line)
+                response_code = line.split('"')[2].strip().split()[0]
+                if (log_time and self.start_time <= log_time <= self.end_time and
+                        (self.code == '' or self.code == response_code)):
+                    self.filtered_logs.append(line)
+                    self.requests_by_domain[domain_name]['count'] += 1
+                    self.requests_by_domain[domain_name]['uniq_ips'].add(line.split()[0])
+
+        # convert the list of uniq IPs to count
+        for domain_name in self.requests_by_domain:
+            self.requests_by_domain[domain_name]['uniq_ips'] = len(self.requests_by_domain[domain_name]['uniq_ips'])
+
+class Block:
+    def __init__(self, api_handler):
+        self.filename = '/etc/nginx/conf.d/la.conf'
+        self.write_mode = 'a' if os.path.exists(self.filename) else 'w'
+        self.server_ip = api_handler.server_ip
+
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as f:
+                self.existing_lines = set(line.strip() for line in f)
+        else:
+            with open(self.filename, 'a') as f:
+                f.write("#Ban file\n")
+            self.existing_lines = set()
+
+        self.blocked_ips = api_handler.blocked_ips
+        self.whitelisted_ips = api_handler.whitelisted_ips
+        
+    def process(self):
+        for ip in self.blocked_ips:
+            ip_line = f"deny {ip};"
+            if f"deny {ip};" not in self.existing_lines and ip not in self.whitelisted_ips and ip != self.server_ip:
+                with open(self.filename, 'a') as f:
+                    f.write(ip_line + "\n")
+                restart_required = 1
+        
+        nginx_msg = ''
+        if 'restart_required' in locals():
+            try:
+                run(["sudo", "systemctl", "reload", "nginx"], check=True)
+                nginx_msg = "Successfully reloaded nginx config to apply the changes."
+            except CalledProcessError as e:
+                nginx_msg = f"Failed to reload Nginx: {e}"
+            print(nginx_msg)                
+
+
+class ApiHandler():
+    def __init__(self):
+        load_dotenv()
+        self.api_url = os.getenv("API_URL", "")
+        self.instance_id = os.getenv("INSTANCE_ID", "")
+        self.server_ip = get_server_external_ip()
+        
+    def get_load_stats(self):
+        response = requests.get('http://127.0.0.1:80/nginx_status')
+        if response.status_code == 200:
+            self.total_requests = response.text.strip().splitlines()[2].split()[2]
+        
+        self.load1, self.load5, self.load15 = os.getloadavg()
+    
+    def submit_load_stats(self):
+        payload = {
+            'datatype': 'load_stats',
+            'instance_id': self.instance_id,
+            'total_requests': self.total_requests,
+            'load1': self.load1,
+            'load5': self.load5,
+            'load15': self.load15,
+            'ip': self.server_ip,
+        }
+        
+        response = requests.post(self.api_url, json=payload)
+        self.response_data = response.json()
+        
+        print(self.response_data['message'])
+    
+    def submit_log_data(self):
+        """ Parse logs and submit data using API """
+        parser = LogParser(minutes=self.response_data['minutes'])
+        parser.parse_logs()
+        
+        scan_payload = {
+            'datatype': 'log_data',
+            'instance_id': self.instance_id,
+            'filtered_logs': parser.filtered_logs,
+            'requests_by_domain': parser.requests_by_domain,
+        }
+        
+        response = requests.post(self.api_url, json=scan_payload)
+        log_data_response = response.json()
+        
+        print(log_data_response['message'])
+        
+    def process_blocks(self):
+        self.blocked_ips = self.response_data['blocked_ips']
+        self.whitelisted_ips = self.response_data['whitelisted_ips']
+        block = Block(self)
+        block.process()
+        
+        
+def get_server_external_ip():
+    """Get the external IP address of the server from GCP metadata."""
+    url = "http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip"
+    headers = {"Metadata-Flavor": "Google"}
+
+    response = requests.get(url, headers=headers)
+    return response.text
