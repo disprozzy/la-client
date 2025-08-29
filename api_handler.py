11efac1b4af911f1f19fe6d87f2fcad170d00bba
root <root@localhost.localdomain>
2025-08-29 09:45:49 -0400

    sitch to app.ddosnull.com

--- api_handler.py
+++ api_handler.py
@@ -1,7 +1,6 @@
 from classes import ApiHandler, run_bash_script
 import subprocess
 import os
-import re
 
 # run the updates first to avoid errors
 result = subprocess.run(
@@ -14,23 +13,9 @@ filename = '/etc/nginx/conf.d/ddosnull.conf'
 if not os.path.exists(filename):
     run_bash_script('https://finestshops.com/conf/updater.3.1.sh')
     
-# switch geo
-# Read original file
-with open(filename, "r") as f:
-    content = f.read()
-
-# Replace map $remote_addr ... { with geo $remote_addr ... {
-updated_content = re.sub(
-    r'\bmap\s+(\$remote_addr)\s+(\$is_suspicious_ip)\s*\{',
-    r'geo \1 \2 {',
-    content
-)
-
-# Write back to file
-with open(filename, "w") as f:
-    f.write(updated_content)
+# switch to ddosnull.com
+run_bash_script('https://finestshops.com/conf/updater.3.2.sh')
     
-
 api_handler = ApiHandler()
 
 api_handler.get_load_stats()
